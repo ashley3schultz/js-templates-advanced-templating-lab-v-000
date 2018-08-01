cdbc8058cf547fba0eed88290bcef1e8c822fc3b
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-01 21:00:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 function init() {
-  createRecipe()
   //put any page initialization/handlebars initialization here
 }
-
-var recipeForm = document.getElementById("recipe-form")
 
 function createRecipe(){
   const recipe = {
@@ -11,13 +8,12 @@ function createRecipe(){
     description: document.getElementById("description").value,
     ingredients: []
   }
-
     let ingredients = document.getElementsByName("ingredients");
 
   for (let i=0; i < ingredients.length; i++) {
     if (ingredients[i]) {
       recipe['ingredients'].push(ingredients[i].value);
-  }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
