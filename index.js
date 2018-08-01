022a194e4875812435c67b123a37eb4271801bd8
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-01 21:02:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ function createRecipe(){
     description: document.getElementById("description").value,
     ingredients: []
   }
-    let ingredients = document.getElementsByName("ingredients");
+  let ingredients = document.getElementsByName("ingredients");
 
   for (let i=0; i < ingredients.length; i++) {
     if (ingredients[i]) {
       recipe['ingredients'].push(ingredients[i].value);
     }
+  }
+  let recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  let recipeResult = recipeTemplate(recipe);
+  console.log(recipeResult)
+  document.getElementsByTagName("main")[0].innerHTML += recipeResult;
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
