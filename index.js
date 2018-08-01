d2f1d8d5f357de893563353eabf9455330e3352b
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-01 21:31:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  createPartials();
+  createHelpers();
+  loadRecipeForm();
 }
-
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
-
 function createPartials() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
-  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
+Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
 }
 
 function createHelpers() {
