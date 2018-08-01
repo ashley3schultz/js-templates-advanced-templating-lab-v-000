f2732be0425566589251d7a021880ddd1213acb9
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-01 20:23:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
-
-Handlebars.registerPartial('namePartial', document.getElementById("partial-template").innerHTML)
-function renderMain() {
-  var template = Handlebars.compile(document.getElementById("main-template").innerHTML);
-  var html = template({name: 'Gordon Ramsay'});
-}
+var recipeForm = document.getElementById("recipe-form")
