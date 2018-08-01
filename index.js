fece488ab1f9283dc8a345fdfc4bf00bd1a03911
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-01 21:51:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
+
 function createPartials() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
 }
 
-function createHelpers() {
+
   Handlebars.registerHelper('displayIngredient', function() {
     if (this != "") {
       return '<li>' + this + '</li>';
     }
   })
-}
+
 
 function loadRecipeForm() {
   const recipeForm = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
@@ -28,6 +29,7 @@ function loadRecipeForm() {
   }
   document.getElementsByTagName("main")[0].innerHTML += recipeForm(context);
 }
+
 
 function createRecipe() {
   const recipe = {
@@ -46,14 +48,16 @@ function createRecipe() {
   document.getElementsByTagName("main")[0].innerHTML += recipeResult
 }
 
+
 function displayEditForm() {
   const editForm = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
   const context = {
     submitAction: "updateRecipe();"
   }
-
   document.getElementsByTagName("main")[0].innerHTML += editForm(context);
 }
+
+
 function updateRecipe() {
   const recipe = {
     name: document.getElementById("name").value,
