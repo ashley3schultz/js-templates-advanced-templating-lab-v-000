048464d0cb070a29eb93c3a5025922ad9bfca1e0
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-01 20:34:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function init() {
   //put any page initialization/handlebars initialization here
 }
+
+var recipeForm = document.getElementById("recipe-form").innerHTML
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
-var recipeForm = document.getElementById("recipe-form").innerHTML
+
